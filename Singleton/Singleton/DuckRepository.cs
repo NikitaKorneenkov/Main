7ed5805424555cfe9aefa44d586e7a41fdb46da4
Nikita Korneenkov <nikita778899kor@gmail.com>
2021-01-02 14:52:05 +0200

    add singleton

---
+++ Singleton/Singleton/DuckRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singleton
+{
+    class DuckRepository
+    {
+        public Dictionary<string, int> Storage;
+
+        private DuckRepository()
+        {
+            init();
+        }
+
+        private void init()
+        {
+            Storage = new Dictionary<string, int>();
+            Console.WriteLine("Единственный экземпляр склада создан...");
+        }
+
+        public void Add(string duckType)
+        {
+            if (Storage.ContainsKey(duckType))
+            {
+                Storage[duckType]++;
+            }
+            else
+            {
+                Storage.Add(duckType, 1);
+            }
+        }
+
+        private static DuckRepository _item;
+
+        public static DuckRepository Get()
+        {
+            if (_item == null)
+            {
+                _item = new DuckRepository();
+            }
+            return _item;
+        }
+    }
+}
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using DuckFarm.Ducks;
+
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DuckRepository duckstorage = DuckRepository.Get();
+            for (int i = 0; i < 4; i++)
+            {
+                Duck duck = new Duck();
+                duckstorage.Add(duck.name);
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                WoodenDuck woodenduck = new WoodenDuck();
+                duckstorage.Add(woodenduck.name);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                RubberDuck rubberduck = new RubberDuck();
+                duckstorage.Add(rubberduck.name);
+            }
+
+            Console.WriteLine("На складе имеется:");
+            foreach (KeyValuePair<string, int> keyValue in duckstorage.Storage)
+            {
+                Console.WriteLine(keyValue.Key + ": " + keyValue.Value + " штук");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
